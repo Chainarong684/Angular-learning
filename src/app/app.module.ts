3892d997ded273d226322a4a0a2f2366fa2748d9
ckdev <9k.chainarong@gmail.com>
2021-06-01 23:37:57 +0700

    ngClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { FormsModule } from '@angular/forms';
 import { NgIfComponent } from './ng-if/ng-if.component';
 import { NgStyleComponent } from './ng-style/ng-style.component';
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { NgClassComponent } from './ng-class/ng-class.component';
 
 @NgModule({
-  declarations: [AppComponent, TwoWayDatabindingComponent, NgIfComponent, NgStyleComponent, PropertyBindingComponent],
+  declarations: [AppComponent, TwoWayDatabindingComponent, NgIfComponent, NgStyleComponent, PropertyBindingComponent, NgClassComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
